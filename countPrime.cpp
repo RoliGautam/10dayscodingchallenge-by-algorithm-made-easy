8516be4deb2ad5b36bc21b5dde350ebdaf66ec1b
Roli Gautam <72242893+RoliGautam@users.noreply.github.com>
2021-05-10 21:49:38 +0530

    Add files via upload

---
+++ countPrime.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countPrimes(int n){
+        if(n<3){
+            return 0;
+        }
+        int count = 1;
+        for(int i = 3; i<n; i= i+2){
+            if(isPrime(i)){
+                count++;
+            }
+        }
+            return count;
+
+    }
+
+    bool isPrime(int n){
+
+        if(n==1)
+		return false;
+
+	if(n==2 || n==3)
+		return true;
+
+	if(n%2==0 || n%3==0)
+		return false;
+
+	for(int i=5; i*i<=n; i=i+6)
+	{
+		if(n % i == 0 || n % (i + 2) == 0)
+			return false;
+	}
+
+	return true;
+    }
+
+
+};
